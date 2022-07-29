f447798e24953089b728b03f2e5e0dd86b8f9d95
DESKTOP-QKSE2O9\Márk <benem960802@gmail.com>
2022-07-29 10:04:49 +0200

    Starting project

---
+++ src/main/java/com/benem/findyourdreamjob/FindyourdreamjobApplication.java
@@ -0,0 +1,13 @@
+package com.benem.findyourdreamjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourdreamjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyourdreamjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benem/findyourdreamjob/FindyourdreamjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benem.findyourdreamjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyourdreamjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
